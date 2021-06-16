c1e2b6d0b6d6bc759b8accf29eb5c82c1928a7f8
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-06-16 12:59:08 -0400

    remove unessential files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 
 const App=()=> {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Task Tracker</h2>
-      </header>
+     <h1>Task Management</h1>
     </div>
   );
 }
